4f0fb21f41e55e2f3cea0dee8f7bd326e770d541
Mikael Lasichuk <mikaellasichuk@MacBook-Pro-Mikael.local>
2021-06-03 22:04:49 +0100

    validation

--- js/index.js
+++ js/index.js
@@ -1,31 +1,29 @@
 const submitButton = document.querySelector('button');
-
 const getNode = (selector) => document.querySelector(selector);
 
 const validate = (email) => {
-  if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email))
-  {
+  if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
+      email)) {
     showModal('Thank you!');
     getNode('form').submit();
-    return (true)
-  }
+    getNode('form').reset();
+  } else {
     showModal('You have entered an invalid email address!');
-  return (false)
+  }
 };
 
 const showModal = (text) => {
   getNode('.message').innerText = text;
   getNode('.modal').style.display = 'block';
   setTimeout(() => {
-    getNode('.modal').style.display = 'none'
-  }, 2000)
+    getNode('.modal').style.display = 'none';
+  }, 2000);
 };
 
 const submit = (e) => {
-  let inputValue = getNode('#email').value;
+  const inputValue = getNode('#email').value;
   e.preventDefault();
   validate(inputValue);
-  getNode('form').reset();
 };
 
 submitButton.addEventListener('click', submit);
